714e010c47b7b732447394f9c8358592548a87b9
Abdi Berhe <abdiberhe@gmail.com>
2023-11-01 23:47:33 +0300

    [Function]: get_logger func

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,7 +8,7 @@ import logging
 from typing import List
 import re
 
-PII_FIELDS = ('email', 'phone', 'ssn', 'password', 'ip')
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str],
