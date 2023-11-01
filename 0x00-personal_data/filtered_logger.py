7942b34efa51d4ee73c60444f1efa852f9b1238d
Abdi Berhe <abdiberhe@gmail.com>
2023-11-01 23:13:30 +0300

    [Class]: Redacting formatter class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ Module: 'filtered_logger'
 Function that returns the log message obfuscated
 '''
 
+import logging
 from typing import List
 import re
 
@@ -18,3 +19,23 @@ def filter_datum(fields: List[str],
                          f'{field}={redaction}{separator}',
                          message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        '''Instantiation'''
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''Filter values in incoming log'''
+        record.message = filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        return super().format(record)
