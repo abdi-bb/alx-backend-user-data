0642d514574474f878597670392ea1f89fb7923a
Abdi Berhe <abdiberhe@gmail.com>
2023-11-02 19:24:50 +0300

    [Update]: Updating the type anotation for the return

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -63,7 +63,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> mysql.connector.connection_cext.CMySQLConnection:
+def get_db() -> mysql.connector.connection.MySQLConnection:
     '''Returns a connector to a db'''
     try:
         db_connection = mysql.connector.connect(
