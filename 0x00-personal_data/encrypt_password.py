e4edc1da6461d7a1289345c376cd8f05daaea011
Abdi Berhe <abdiberhe@gmail.com>
2023-11-02 20:58:46 +0300

    [Function]: That validates password with hashed password using bcrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,8 @@ def hash_password(password: str) -> bytes:
     '''Encrypts password'''
     password = password.encode('utf-8')
     return bcrypt.hashpw(password, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''Checks if hashed_password and password are valid to eache other'''
+    return bcrypt.checkpw(password.encode(), hashed_password)
