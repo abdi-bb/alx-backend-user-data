04a175437c3c75c9ee3707e39b33e4f46f4f3ee2
Abdi Berhe <abdiberhe@gmail.com>
2023-11-08 02:03:55 +0300

    [Update]: Filter each request

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -45,19 +45,20 @@ def forbidden(error) -> str:
 
 
 @app.before_request
-def before_request():
+def before_request() -> str:
     '''Filters each request
     '''
-    if auth:
-        excluded_paths = ['/api/v1/status/',
-                          '/api/v1/unauthorized/',
-                          '/api/v1/forbidden/']
-        req_auth = auth.require_auth(request.path, excluded_paths)
-        if req_auth:
-            if auth.authorization_header(request) is None:
-                abort(401)
-            if auth.current_user(request) is None:
-                abort(403)
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/',
+                        '/api/v1/unauthorized/',
+                        '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
