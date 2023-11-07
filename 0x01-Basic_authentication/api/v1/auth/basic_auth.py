bddfa88daaa5ab4dde989b792cadd862a2ed85cf
Abdi Berhe <abdiberhe@gmail.com>
2023-11-08 02:51:12 +0300

    [Class]: Class BasicAuth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -23,7 +23,8 @@ class BasicAuth(Auth):
         return authorization_header[6:]
 
     def decode_base64_authorization_header(self,
-                                           base64_authorization_header: str) -> str:
+                                           base64_authorization_header: str
+                                           ) -> str:
         '''Returns Decoded value of Base64 string
         base64_authorization_header
         '''
