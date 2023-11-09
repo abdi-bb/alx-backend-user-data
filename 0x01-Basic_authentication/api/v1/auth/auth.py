d88860852488e83ab1c268153ac0f228a7d0c5e2
Abdi Berhe <abdiberhe@gmail.com>
2023-11-09 09:52:59 +0300

    [Update]: Remove unnecessary comments

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -37,15 +37,6 @@ class Auth():
                     return False
 
         return True
-        '''for excluded_path in excluded_paths:
-            if excluded_path.endswith('*'):
-                # Remove the trailing wildcard character for comparison
-                excluded_path = excluded_path[:-1]
-
-            if path.startswith(excluded_path):
-                return False
-
-        return True'''
 
     def authorization_header(self, request=None) -> str:
         '''Returns Authorization header
