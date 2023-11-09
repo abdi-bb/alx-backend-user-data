608983d78ee78021eb54b4968da8113ff3e91f7b
Abdi Berhe <abdiberhe@gmail.com>
2023-11-09 15:27:52 +0300

    [Feauture]: Returns the logged in user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,6 +6,7 @@ SessionAuth class to manage the API authentication
 from api.v1.auth.auth import Auth
 from uuid import uuid4
 import os
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -26,9 +27,8 @@ class SessionAuth(Auth):
             return None
         return self.user_id_by_session_id.get(session_id)
 
-    def session_cookie(self, request=None):
-        '''Returns cookie value from the request'''
-        if request is None:
-            return None
-        session_cookie_name = os.getenv('SESSION_NAME')
-        return request.cookies.get(session_cookie_name)
+    def current_user(self, request=None):
+        '''Identify a user using Session ID'''
+        session_id = self.session_id(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
