9a7fb555862369b7fcd9ad06e6224da001155e8b
Abdi Berhe <abdiberhe@gmail.com>
2023-11-09 13:27:06 +0300

    [Feauture]: Extracts user_id using session_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -15,6 +15,12 @@ class SessionAuth(Auth):
         '''Creates a Session ID for a user_id'''
         if user_id is None or not isinstance(user_id, str):
             return None
-        session = str(uuid4())
-        self.user_id_by_session_id[session] = user_id
-        return session
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        '''Returns user_id based on the session_id'''
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
