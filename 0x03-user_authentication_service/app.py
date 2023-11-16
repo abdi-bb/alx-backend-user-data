80a782c60931a1867cf94960fbb623a1d9117045
Abdi Berhe <abdiberhe@gmail.com>
2023-11-17 02:40:25 +0300

    [Function]: Endpoint for updating password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -76,5 +76,19 @@ def get_reset_password_token():
     return jsonify({"email": user.email, "reset_token": pwd_reset_token}), 200
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    '''Endpoint for /reset_password with PUT'''
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_password')
+    new_password = request.form.get('new_password')
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
