87446ac9b9ac9a9e71029671866c548091bcb653
Abdi Berhe <abdiberhe@gmail.com>
2023-11-16 23:45:49 +0300

    [Feature]: Valid_login method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -41,6 +41,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             return False
-        if user and bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+        if user and bcrypt.checkpw(password.encode('utf-8'),
+                                   user.hashed_password):
             return True
         return False
