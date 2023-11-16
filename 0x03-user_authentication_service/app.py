8d2c5ad421c71169cd35203d38809cee5469b8a5
Abdi Berhe <abdiberhe@gmail.com>
2023-11-16 20:57:58 +0300

    [Update]: Coorect the accepted methods

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/', methods='GET', strict_slashes=False)
+@app.route('/', methods=['GET'], strict_slashes=False)
 def welcome():
     return jsonify({"message": "Bienvenue"})
 
